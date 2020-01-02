50c12651a4d7a5bd9c0618a263b4f10ae5e7a619
Syed Hammad Jan <hammad@hadiftech.com>
2020-01-02 08:47:13 +0500

    Initial Commit

---
+++ MVVMCountryList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMCountryList
+//
+//  Created by syed hammad jan on 02/01/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVVMCountryListTests/MVVMCountryListTests.swift
@@ -0,0 +1,33 @@
+//
+//  MVVMCountryListTests.swift
+//  MVVMCountryListTests
+//
+//  Created by syed hammad jan on 02/01/2020.
+//
+
+import XCTest
+@testable import MVVMCountryList
+
+class MVVMCountryListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
